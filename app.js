5ac956c1371fb6e59ed922373d6c70caafd894f6
sjon de gast <>
2018-08-28 05:42:17 +0200

    add campground template, forEach content

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/campgrounds", function(req, res) {
         { name: "Granite Hill", image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144290f3c97aaee8b5_340.jpg"},
         { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f5c271a4e4b0b8_340.jpg"}
     ];
-    res.render("campgrounds");
+    res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.listen(8080, "localhost", function () {
