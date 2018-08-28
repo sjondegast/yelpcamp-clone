0a6a6ef82fdfbc722f0f1fe070e4e9e1084f3847
sjon de gast <>
2018-08-28 05:12:26 +0200

    added /campgrounds route, ejs and array of campgrounds

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var app = express();
 
+
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-    res.send("This will be the landing page soon!");    
-})
+    res.render("landing");   
+});
+
+app.get("/campgrounds", function(req, res) {
+    var campgrounds = [
+        { name: "Salmon Creek", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104496f5c271a4e4b0b8_340.jpg"},
+        { name: "Granite Hill", image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144290f3c97aaee8b5_340.jpg"},
+        { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f5c271a4e4b0b8_340.jpg"}
+    ];
+    res.render("campgrounds");
+});
 
 app.listen(8080, "localhost", function () {
     console.log("YelpCamp Server Has Started");
--- logbook.md
+++ logbook.md
@@ -3,7 +3,25 @@ npm install express ejs --save #install express and ejs on the same time
 make a .gitignore file, this excludes node modules: touch .gitignore && echo "node_modules/" >> .gitignore
 If you allready added the node modules you can run the following: git rm -r --cached node_modules && touch .gitignore && echo "node_modules/" >> .gitignore
 
-touch app.js
+touch app.js and do basic setup:
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("This will be the landing page soon!");    
+})
+
+app.listen(8080, "localhost", function () {
+    console.log("YelpCamp Server Has Started");
+});
+
+npm install nodemon --save
+mkdir views + touch views/landing.ejs
+
+Inside app.js set up view engine ejs, this way no .ejs required.
+app.set("view engine", "ejs");
+
+
 
 
 ----- Q&A ----
