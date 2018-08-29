fc6eaa525b05165399d0e3ba63ec859825cd829c
sjon de gast <>
2018-08-29 05:24:50 +0200

    POST request, get name + image from form and push them into array, moved the array to global scope and redirect to /campgrounds

--- app.js
+++ app.js
@@ -5,23 +5,28 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+var campgrounds = [
+    { name: "Salmon Creek", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/meilleurs-choix-camping-parcs.jpg" },
+    { name: "Granite Hill", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/meilleurs-choix-camping-parcs.jpg" },
+    { name: "Mountain Goat's Rest", image: "https://s3.amazonaws.com/imagescloud/images/cards/camping/meilleurs-choix-camping-parcs.jpg" }
+];
+
 app.get("/", function (req, res) {
     res.render("landing");   
 });
 
 app.get("/campgrounds", function(req, res) {
-    var campgrounds = [
-        { name: "Salmon Creek", image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104496f5c271a4e4b0b8_340.jpg"},
-        { name: "Granite Hill", image: "https://pixabay.com/get/e03db50f2af41c22d2524518b7444795ea76e5d004b0144290f3c97aaee8b5_340.jpg"},
-        { name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f5c271a4e4b0b8_340.jpg"}
-    ];
     res.render("campgrounds", {campgrounds: campgrounds});
 });
 
 app.post("/campgrounds", function(req, res){
-    res.send("you entered the post route");
     //get data from form and add to campground array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
     //redirect back to campgrounds page
+    res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function (req, res) {
