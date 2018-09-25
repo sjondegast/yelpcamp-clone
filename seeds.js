3b2864c305e235ccb9b0be27bbb5acea4576f82e
sjon de gast <>
2018-09-25 14:38:55 +0200

    moved adding campgrounds from data array inside the if else of the remove all campgrounds function and now it will only run wen all the campgrounds are removed and not before

--- seeds.js
+++ seeds.js
@@ -1,8 +1,7 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
 
-var data = [
-    {
+var data = [{
         name: "Clouds's Rest",
         image: "https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=053f91dd9aee1cc7bc5cafca28cb625c&auto=format&fit=crop&w=500&q=60",
         description: "blah blah blah"
@@ -17,7 +16,7 @@ var data = [
         image: "https://images.unsplash.com/photo-1477581265664-b1e27c6731a7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=74b892f0ae4c8ca51497e2ec4d1aefba&auto=format&fit=crop&w=500&q=60",
         description: "test this option as well"
     }
-];  
+];
 
 function seedDB() {
     //Remove all campgrounds
@@ -26,22 +25,20 @@ function seedDB() {
             console.log(err);
         } else {
             console.log("all campgrounds removed from database!!");
+            //add a few campgrounds
+            data.forEach(function (seed) {
+                Campground.create(seed, function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("added a campground");
+                        console.log(data);
+                    }
+                });
+            });
         }
     });
-    //add a few campgrounds
-    data.forEach(function (seed) {
-        Campground.create(seed, function (err, data) {
-           if (err) {
-               console.log(err);
-           } else {
-               console.log("added a campground");
-               console.log(data);
-           }
-        });
-    });
-
     //add a few comments
 };
 
 module.exports = seedDB;
-
