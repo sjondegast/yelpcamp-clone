3b402382d912db9a23d0fc66342635e6bfdda512
sjon de gast <>
2018-09-30 21:40:47 +0200

    added button to show template for adding a comment, inside the app.js file the post route for the comments push the new comment to the comments inside the campground + id and saved the comment to the db and redirect to the campgrounds show page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express         = require("express"),
     bodyParser      = require("body-parser"),                   
     mongoose        = require("mongoose"),
     Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
     seedDB          = require("./seeds");
    
 mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
@@ -83,6 +84,22 @@ app.get("/campgrounds/:id/comments/new", function (req, res) {
 
 app.post("/campgrounds/:id/comments", function (req, res) {
     //look up campground using id
+    Campground.findById(req.params.id, function (err, campground) {
+        if (err) {
+            console.log(err);
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+               if (err) {
+                   console.log(err);
+               } else {
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect("/campgrounds/" + campground._id);
+               }
+            });
+        }
+    });
     //create new comment
     //connect new comment to campground
     //redirect to campground showpage
