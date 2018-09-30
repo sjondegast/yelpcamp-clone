acf191385753e5b47a7b7ed04c1be4d1d861f5a3
sjon de gast <>
2018-09-30 22:08:02 +0200

    added more lorem ipsum inside the seedsfile in the description of campgrounds so that we have content to style

--- seeds.js
+++ seeds.js
@@ -5,17 +5,17 @@ var Comment = require("./models/comment");
 var data = [{
         name: "Clouds's Rest",
         image: "https://images.unsplash.com/photo-1525209149972-1d3faa797c3c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=053f91dd9aee1cc7bc5cafca28cb625c&auto=format&fit=crop&w=500&q=60",
-        description: "blah blah blah"
+        description: "Lorem ipsum dolor amet snackwave franzen disrupt, try-hard hexagon vinyl cronut brooklyn cloud bread mumblecore marfa fixie hot chicken. Cray adaptogen iceland tofu palo santo shaman 3 wolf moon. Next level intelligentsia small batch cornhole tattooed blog. YOLO vexillologist vice, pug street art synth tumblr typewriter gochujang hexagon twee jean shorts. Sartorial jean shorts prism, narwhal kitsch tilde bitters ramps small batch artisan. Cronut bushwick photo booth, 90's etsy tofu humblebrag prism."
     },
     {
         name: "Desert Mesa",
         image: "https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=fcbebfe204ad7e04d558d7e0cbc0d2eb&auto=format&fit=crop&w=500&q=60",
-        description: "blah blah test this fucker"
+        description: "Lorem ipsum dolor amet snackwave franzen disrupt, try-hard hexagon vinyl cronut brooklyn cloud bread mumblecore marfa fixie hot chicken. Cray adaptogen iceland tofu palo santo shaman 3 wolf moon. Next level intelligentsia small batch cornhole tattooed blog. YOLO vexillologist vice, pug street art synth tumblr typewriter gochujang hexagon twee jean shorts. Sartorial jean shorts prism, narwhal kitsch tilde bitters ramps small batch artisan. Cronut bushwick photo booth, 90's etsy tofu humblebrag prism."
     },
     {
         name: "Canyon flour",
         image: "https://images.unsplash.com/photo-1477581265664-b1e27c6731a7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=74b892f0ae4c8ca51497e2ec4d1aefba&auto=format&fit=crop&w=500&q=60",
-        description: "test this option as well"
+        description: "Lorem ipsum dolor amet snackwave franzen disrupt, try-hard hexagon vinyl cronut brooklyn cloud bread mumblecore marfa fixie hot chicken. Cray adaptogen iceland tofu palo santo shaman 3 wolf moon. Next level intelligentsia small batch cornhole tattooed blog. YOLO vexillologist vice, pug street art synth tumblr typewriter gochujang hexagon twee jean shorts. Sartorial jean shorts prism, narwhal kitsch tilde bitters ramps small batch artisan. Cronut bushwick photo booth, 90's etsy tofu humblebrag prism."
     }
 ];
 
