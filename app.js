0cbd3c2dad106246eaa15caf3e61bce6615f4f45
sjon de gast <>
2018-10-02 17:48:16 +0200

    got the user from req.user and then set up app.use so that it will be avalable in every template and then did a if statement to show the right buttons inside the header template - were the login, signup and signout are...

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
+
 app.get("/", function (req, res) {
     res.render("landing");   
 });
@@ -38,7 +43,7 @@ app.get("/campgrounds", function(req, res) {
        if (err) {
            console.log(err);           
        } else {
-           res.render("campgrounds/index", {campgrounds: campgrounds});
+           res.render("campgrounds/index", {campgrounds: campgrounds, user: req.user});
        }
     });
 });
