614026a71a507d93b08515c32f2f5059da50795c
sjon de gast <>
2018-10-02 04:55:33 +0200

    added register.ejs to views dir and added a form for signing up, then set up the get route in app.js and the post route for the form to handle the request

--- app.js
+++ app.js
@@ -117,6 +117,30 @@ app.post("/campgrounds/:id/comments", function (req, res) {
     });
 });
 
+//===============
+// AUTH ROUTES
+//===============
+
+// show register form
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+//handle sign up logic
+app.post("/register", function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/campgrounds");     
+        });
+    });
+});
+
+
 app.listen(8080, "localhost", function () {
     console.log("YelpCamp Server Has Started");
 });
