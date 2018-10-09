253bb46d54387b23ad8af6306ad0a1451408e4bd
sjon de gast <>
2018-10-09 04:57:52 +0200

    added delete route for comments

--- routes/comments.js
+++ routes/comments.js
@@ -65,7 +65,13 @@ router.put("/:comment_id", function (req, res) {
 });
 
 router.delete("/:comment_id", function (req, res) {
-   findByIdAndRemove() 
+   Comment.findByIdAndRemove(req.params.comment_id, function (err) {
+       if (err) {
+           res.redirect("back");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   }); 
 });
 
 //MIDDLEWARE
