8f551d94ba65749684745a85864fe65ef890c53a
sjon de gast <>
2018-10-09 21:06:30 +0200

    stalled flash-connect and require + app.use in app.js

--- app.js
+++ app.js
@@ -8,18 +8,21 @@ var express         = require("express"),
     Campground      = require("./models/campground"),
     Comment         = require("./models/comment"),
     User            = require("./models/user"),
+    flash           = require("connect-flash"),
     seedDB          = require("./seeds");
 
 //REQUIRING ROUTES
 var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index");
-   
+
+// APP CONFIGURATION
 mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB();
 
 // PASSPORT CONFIGURATION
